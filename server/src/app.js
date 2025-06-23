d0e6be665ab716a3b55c28031df222d896c07661
David Chiedu <dedestunting@gmail.com>
2025-06-23 10:44:29 -0700

    Initial Bundo backend setup

--- server/src/app.js
+++ server/src/app.js
@@ -1,31 +1,16 @@
-// server/src/app.js
-const dotenv = require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const mongoose = require('mongoose');
-
-const connectDB = require('../config/db');
-
-const artisanRoutes = require('./routes/artisanRoutes');
-const bookingRoutes = require('./routes/bookingRoutes');
-const adminRoutes = require('./routes/adminRoutes');
-
-
-connectDB();
+require('dotenv').config();
 
 const app = express();
-
 app.use(cors());
 app.use(express.json());
 
-// Routes
-app.use('/api/artisans', artisanRoutes);
-app.use('/api/bookings', bookingRoutes);
-app.use('/api/admin', adminRoutes);
-
 app.get('/', (req, res) => {
-  res.send('Bundo Marketplace API running...');
+  res.send('Bundo API is running...');
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
